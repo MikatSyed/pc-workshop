da486209fd596107209938e338df809ebe1211bf
unknown <syednew5000@gmail.com>
2023-07-30 17:20:56 +0600

    [done] add redux persist to handle persistency on selected product

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,16 +1,22 @@
 import "@/styles/globals.css";
 import { SessionProvider } from "next-auth/react";
 import { Provider } from "react-redux";
-import { store } from "@/redux/store";
+import { persistor, store } from "@/redux/store";
+import { PersistGate } from 'redux-persist/integration/react';
 
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
+  const getLayout = Component.getLayout ?? ((page) => page);
 
-
-export default function App({ Component, pageProps: { session, ...pageProps } }) {
-    const getLayout = Component.getLayout ?? ((page) => page);
-
-    return (
-        <Provider store={store}>
-            <SessionProvider session={session}>{getLayout(<Component {...pageProps} />)}</SessionProvider>
-        </Provider>
-    );
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <SessionProvider session={session}>
+          {getLayout(<Component {...pageProps} />)}
+        </SessionProvider>
+      </PersistGate>
+    </Provider>
+  );
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,16 @@
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
 import { configureStore } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist";
 import pcBuildersReducer from "./features/PcBuildSlice";
+
+const persistConfig = { key: "root", storage };
+const persistedReducer = persistReducer(persistConfig, pcBuildersReducer);
 
 export const store = configureStore({
     reducer: {
-        pcBuilder: pcBuildersReducer,
+        pcBuilder: persistedReducer,
     },
-   
 });
+
+export const persistor = persistStore(store);
 
